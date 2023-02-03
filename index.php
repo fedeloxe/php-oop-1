3b1a9ba95cffff88344cf7165c5a1dd8cca229e4
fedeloxe <loxeee@gmail.com>
2023-02-03 16:22:44 +0100

    echo done

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ class Movie
 }
 
 $movie1 = new Movie('La Haine', 'Francia', '1995', '95 minuti', 'Mathieu Kassovitz');
-$moviu2 = new Movie('Inglourious Basterds', 'USA Germania', '2009', '153 minuti', 'Quentin Tarantino');
+$movie2 = new Movie('Inglourious Basterds', 'USA, Germania', '2009', '153 minuti', 'Quentin Tarantino');
 
 ?>
 
@@ -32,10 +32,29 @@ $moviu2 = new Movie('Inglourious Basterds', 'USA Germania', '2009', '153 minuti'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
+    <div class="container">
+        <div class="film1">
+            <h1><?php echo  $movie1->titolo; ?> </h1>
+            <h3><?php echo $movie1->paese_produzione ?></h3>
+            <h3><?php echo $movie1->anno ?></h3>
+            <h3><?php echo $movie1->durata ?></h3>
+            <h3><?php echo $movie1->regista ?></h3>
+        </div>
+        <div class="film-2 pt-5">
+            <h1><?php echo $movie2->titolo; ?> </h1>
+            <h3><?php echo $movie2->paese_produzione ?></h3>
+            <h3><?php echo $movie2->anno ?></h3>
+            <h3><?php echo $movie2->durata ?></h3>
+            <h3><?php echo $movie2->regista ?></h3>
+
+        </div>
+
+    </div>
 
 </body>
 
